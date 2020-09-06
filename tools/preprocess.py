da0a7db8662d42645df8c0f3a0330b71d9b63371
Maxwell <maxwell.acioli@gmail.com>
2020-09-06 12:52:02 -0300

    Add abono dataset as dataframe

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -11,25 +11,31 @@ import numpy as np
 # import datetime as dt
 
 # def preprocess():
-aposentados_df1 = pd.read_csv('../datasets/dataset-aposentados/APOSENTADOS_052020_FULL_PARTE_1.csv', sep=';', encoding='ISO-8859-1', dtype='unicode')
-aposentados_df2 = pd.read_csv('../datasets/dataset-aposentados/APOSENTADOS_052020_FULL_PARTE_2.csv', sep=';', encoding='ISO-8859-1', dtype='unicode')
 
-dataframes = [aposentados_df1, aposentados_df2]
+'''#### LOAD APOSENTADOS DATASET ####'''
+# aposentados_df1 = pd.read_csv('../datasets/dataset-aposentados/APOSENTADOS_052020_FULL_PARTE_1.csv', sep=';', encoding='ISO-8859-1', dtype='unicode')
+# aposentados_df2 = pd.read_csv('../datasets/dataset-aposentados/APOSENTADOS_052020_FULL_PARTE_2.csv', sep=';', encoding='ISO-8859-1', dtype='unicode')
 
-aposentados_df = pd.concat(dataframes)
+# dataframes = [aposentados_df1, aposentados_df2]
 
-row_index_column_error2_not_nan =  np.where(aposentados_df['Column Error2'].notnull())[0]
+# aposentados_df = pd.concat(dataframes)
 
-aposentados_df = aposentados_df.drop(row_index_column_error2_not_nan)
+# row_index_column_error2_not_nan =  np.where(aposentados_df['Column Error2'].notnull())[0]
 
-aposentados_df = aposentados_df.drop(columns=['Column Error2'])
+# aposentados_df = aposentados_df.drop(row_index_column_error2_not_nan)
 
-row_index_column_error1_not_nan =  np.where(aposentados_df['Column Error1'].notnull())[0]
+# aposentados_df = aposentados_df.drop(columns=['Column Error2'])
 
-for i in row_index_column_error1_not_nan:
-    if i in aposentados_df.index:
-        aposentados_df = aposentados_df.drop(i)
+# row_index_column_error1_not_nan =  np.where(aposentados_df['Column Error1'].notnull())[0]
+
+# for i in row_index_column_error1_not_nan:
+#     if i in aposentados_df.index:
+#         aposentados_df = aposentados_df.drop(i)
     
-aposentados_df = aposentados_df.drop(columns=['Column Error1'])
+# aposentados_df = aposentados_df.drop(columns=['Column Error1'])
 
+
+'''#### LOAD ABONO DATASET ####'''
+
+abono_df = pd.read_csv('../datasets/dataset-abono-permanencia/ABONOP_062020.csv', sep=';', encoding='ISO-8859-1', dtype='unicode')
 # return aposentados_df
