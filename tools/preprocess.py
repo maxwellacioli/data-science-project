3f6b2b23de2dc334c43e5bfde552259b59a2aebc
Maxwell <maxwell.acioli@gmail.com>
2020-09-06 21:16:31 -0300

    Create interest dataframe from abono and aposentados datasets

--- tools/preprocess.py
+++ tools/preprocess.py
@@ -42,4 +42,45 @@ aposentados_docentes_df = aposentados_df[aposentados_df['Cargo emprego'].str.con
 abono_df = pd.read_csv('../datasets/dataset-abono-permanencia/ABONOP_062020.csv', index_col=False, sep=';', encoding='ISO-8859-1', dtype='unicode')
 
 abono_docentes_df = abono_df[abono_df['Descrição do cargo emprego'].str.contains("PROFESSOR")]
+
+'''#### CREATE A NEW DATAFRAME FROM ABONO AND APOSENTADOS DATASETS ####'''
+
+'''ABONDO'''
+abono_name = abono_docentes_df['Nome'].tolist()
+abono_cpf = abono_docentes_df['CPF'].tolist()
+abono_cargo = abono_docentes_df['Descrição do cargo emprego'].tolist()
+abono_orgao = abono_docentes_df['Denominação unidade organizacional'].tolist()
+# abono_uf = abono_docentes_df['UF da Redidência'].tolist()
+abono_valor = abono_docentes_df['Val'].tolist()
+
+abono_aux_df = pd.DataFrame()
+abono_aux_df['Nome'] = abono_name
+abono_aux_df['CPF'] = abono_cpf
+abono_aux_df['Cargo'] = abono_cargo
+abono_aux_df['Orgao'] = abono_orgao
+abono_aux_df['Valor'] = abono_valor
+abono_aux_df['Afastamento'] = 1
+
+'''APOSENTADOS'''
+aposentados_nome = aposentados_docentes_df['Nome'].tolist()
+# aposentados_cpf = aposentados_docentes_df['CPF'].tolist()
+aposentados_cargo = aposentados_docentes_df['Cargo emprego'].tolist()
+aposentados_orgao = aposentados_docentes_df['Orgao'].tolist()
+# aposentados_uf = aposentados_docentes_df[''].tolist()
+aposentados_valor = aposentados_docentes_df['Valor aposentadoria'].tolist()
+
+aposentado_aux_df = pd.DataFrame()
+aposentado_aux_df['Nome'] = aposentados_nome
+# aposentado_aux_df['CPF'] = aposentados_cpf
+aposentado_aux_df['Cargo'] = aposentados_cargo
+aposentado_aux_df['Orgao'] = aposentados_orgao
+aposentado_aux_df['Valor'] = aposentados_valor
+aposentado_aux_df['Afastamento'] = 2
+
+dataframes = [abono_aux_df, aposentado_aux_df]
+
+abono_aposentados_df = pd.concat(dataframes)
+
+# professor_abono = abono_docentes_df[abono_docentes_df['Nome'].str.contains("EDSON DOS SANTOS MARCHIORI")]
+# professor_aposentadoria = aposentados_docentes_df[aposentados_docentes_df['Nome'].str.contains("EDSON DOS SANTOS MARCHIORI")]
 # return aposentados_df
